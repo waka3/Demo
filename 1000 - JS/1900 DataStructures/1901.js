e732a70ab4eca30d1489e2a6e1ab0fb91371cb60
waka3 <a420926205@163.com>
2021-12-08 11:22:26 +0800

    feat: 1900

---
+++ 1000 - JS/1900 DataStructures/1901.js
@@ -0,0 +1,91 @@
+
+/**
+ * @function DoublyLinkedList
+ * @description 双向链表
+ * @param nodes {Array{Object}} 节点元素数组
+ */
+class DoublyLinkedList{
+  constructor() {
+    // item { object }
+    this.nodes = [
+      // { value: 值, next: 上个节点, previous: 下个节点 }
+    ]
+  }
+  get size() {
+    return this.nodes.length
+  }
+  get head() {
+    return this.size ? this.nodes[0] : null
+  }
+  get tail() {
+    return this.size ? this.nodes[this.size-1] : null
+  }
+  /**
+   * 检索索引处的元素
+   */
+  getAt(index) {
+    return this.nodes[index]
+  }
+  /**
+   * 清空双向链表
+   */
+  clear() {
+    this.nodes = []
+  }
+  /**
+   * 在特定的索引插入一个元素
+   */
+  insertAt(index, value) {
+    const previous = this.nodes[index - 1] || null
+    const next = this.nodes[index] || null
+    const node = { value, previous, next }
+    if (previous) previous.next = node
+    if(next) next.previous = node
+    this.nodes.splice(index, 0, node)
+  }
+  insertAtFirst(value) {
+    this.insertAt(0, value)
+  }
+  insertAtLast(value) {
+    this.insertAt(this.size, value)
+  }
+  /**
+   * 移除特定索引处的元素
+   */
+  removeAt(index) {
+    const previous = this.nodes[index - 1] || null
+    const next = this.nodes[index] || null
+    if (previous) previous.next = next
+    if(next) next.previous = previous
+    return this.nodes.splice(index, 1)
+  }
+  removeAtFirst() {
+    this.removeAt(0)
+  }
+  removeAtLast() {
+    this.removeAt(this.size - 1)
+  }
+  /**
+   * 清空双向链表
+   */
+  reverse() {
+    this.nodes = this.nodes.reduce((acc, cur, idx, init) => {
+      let node = acc.length ? acc[0] : null
+      cur = { value: cur.value, previous: null, next: node }
+      if (node) {
+        node.previous = cur
+      }
+      return [cur, ...acc]
+    }, [])
+  }
+}
+
+const list = new DoublyLinkedList()
+list.insertAtFirst('AA')
+list.insertAt(1,'A')
+list.insertAt(2,'B')
+list.insertAt(3,'C')
+list.insertAt(4,'D')
+list.insertAtLast('DD')
+list.reverse()
+console.log('list', list.nodes.map(item=> item.value))
---
+++ 1000 - JS/1900 DataStructures/1902.js
@@ -0,0 +1,38 @@
+/**
+ * @function LinkedList
+ * @description JS迭代器（ES6引入）
+ * @param nodes {Array} 节点元素数组
+ * for (let value of [Symbol.iterator]) {} -> TypeError: myList is not iterable
+ * for (let prop/index in 枚举对象)
+ * 一、可迭代协议规定可迭代行为：可迭代对象必须实现 @@iterator 方法
+ * [Symbol.iterator](){} 无参，返回值为符合迭代器协议的对象
+ * 二、迭代器协议规定迭代器行为：需要实现拥有以下语义的 next() 方法
+ * next(){
+ *  // done：true 迭代完毕, false 可以产生序列中的下一个值
+ *  return {done: boolean, value: done为true时被忽略}
+ * }
+ */
+class LinkedList {
+  constructor(data) {
+    this.data = data
+    this.index = 0
+  }
+  [Symbol.iterator]() {
+    return {
+      next: () => {
+        const key = this.index
+        if(this.index < this.data.length ) {
+          this.index++
+          return { done: false, value: this.data[key]}
+        }
+        return { done: true, value: null}
+      }
+    }
+  }
+}
+// const myList = new LinkedList([1, 2, 3, 4]);
+const myList = new LinkedList([{ val: 1 }, { val: 2 }, { val: 3 }]);
+
+for (let item of myList) {
+  console.log(item); // { val: 1 }, { val: 2 }, { val: 3 }
+}
