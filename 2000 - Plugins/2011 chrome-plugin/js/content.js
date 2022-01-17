fbaa42c5ad1f72e8c1bd2038eb29950a643650f5
waka3 <a420926205@163.com>
2022-01-17 15:36:16 +0800

    feat: chrome插件导出dom

--- 2000 - Plugins/2011 chrome-plugin/js/content.js
+++ 2000 - Plugins/2011 chrome-plugin/js/content.js
@@ -1,72 +1,20 @@
 chrome.extension.onRequest.addListener( //监听扩展程序进程或内容脚本发送的请求
   function (request, sender, sendResponse) {
-      console.log(request, sender, sendResponse)
-      if (request.action ==  "autogeturl" ) {
-        window.code = []
-        const texts = document.querySelectorAll('.content')[1].innerText.split('\n')
-        for (let i = 0; i < texts.length; i++){
-          const item = aList[i]
-          const ctns = item.split('：')
-          const obj = {
+    if (request.action ==  "autogeturl" ) {
+      var aList = []
+      var texts = document.querySelectorAll('.talk-content-container .content')[0].innerText.split('\n')
+      for (let i = 0; i < texts.length; i++){
+        var item = texts[i]
+        var ctns = item.split('https://t.zsxq.com')
+        if (ctns.length > 1) {
+          var obj = {
             title: ctns[0],
-            url: ctns[1]
+            url: 'https://t.zsxq.com' + ctns[1]
           }
-          window.code.push(obj)
+          aList.push(obj)
         }
       }
+      exportExcel(aList)
     }
+  }
 );
-
-function openDownloadDialog(url, saveName) {
-  if (typeof url == 'object' && url instanceof Blob) {
-    url = URL.createObjectURL(url); // 创建blob地址
-  }
-  var aLink = document.createElement('a');
-  aLink.href = url;
-  aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
-  var event;
-  if (window.MouseEvent) {
-    event = new MouseEvent('click');
-  } else {
-    event = document.createEvent('MouseEvents');
-    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-  }
-  aLink.dispatchEvent(event);
-}
-// 将一个sheet转成最终的excel文件的blob对象，然后利用URL.createObjectURL下载
-function sheet2blob(sheet, sheetName) {
-  sheetName = sheetName || 'sheet1';
-  var workbook = {
-    SheetNames: [sheetName],
-    Sheets: {}
-  };
-  workbook.Sheets[sheetName] = sheet;
-  // 生成excel的配置项
-  var wopts = {
-    bookType: 'xlsx', // 要生成的文件类型
-    bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
-    type: 'binary'
-  };
-  var wbout = XLSX.write(workbook, wopts);
-  var blob = new Blob([s2ab(wbout)], { type: "application/octet-stream" });
-  // 字符串转ArrayBuffer
-  function s2ab(s) {
-    var buf = new ArrayBuffer(s.length);
-    var view = new Uint8Array(buf);
-    for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
-    return buf;
-  }
-  return blob;
-}
-
-function exportExcel() {
-  const excelItems = [];
-  for (let i = 0; i < window.code.length; i++) {
-    excelItems.push({
-      "标题": window.code[i].title,
-      "链接": window.code[i].url,
-    }); //属性
-  }
-  const sheet = XLSX.utils.json_to_sheet(excelItems);
-  openDownloadDialog(sheet2blob(sheet), '.xlsx');
-}
--- 2000 - Plugins/2011 chrome-plugin/pages/main.js
+++ 2000 - Plugins/2011 chrome-plugin/pages/main.js
@@ -1,12 +1,9 @@
 (function () {
-    document.getElementById('autogeturl-btn').onclick = function() {
-        var url = document.getElementById('autogeturl-id').value
-        if (url === '') {
-            alert('请输入获取链接的地址')
-        }
-        chrome.tabs.getSelected( null ,  function  (tab) { //获取当前tab
-            //向tab发送请求
-            chrome.tabs.sendRequest(tab.id, { action:  "autogeturl", url: url  })
-        })
-    }
+  document.getElementById('autogeturl-btn').onclick = function () {
+    console.log('点击按钮')
+    chrome.tabs.getSelected( null ,  function  (tab) { //获取当前tab
+      //向tab发送请求
+      chrome.tabs.sendRequest(tab.id, { action:  "autogeturl" })
+    })
+  }
 })();
