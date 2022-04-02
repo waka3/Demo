a9deceff4da8570fe6eb40190f9ec75f09f8bea3
waka3 <a420926205@163.com>
2022-04-02 15:59:25 +0800

    feat(plugins): json和excel互转

--- Plugins/uploadExcel/excel.js
+++ Plugins/uploadExcel/excel.js
@@ -1,73 +1,55 @@
 Vue.component('upload-excel-component', {
-  template: `<input ref="excel-upload-input" type="file" accept=".xlsx, .xls" @change="handleClick">`,
-    props: {
-      beforeUpload: Function, // eslint-disable-line
-      onSuccess: Function// eslint-disable-line
+  template: `<input ref="excel-upload-input" type="file" @change="handleClick">`,
+  props: {
+    beforeUpload: Function, // eslint-disable-line
+    onSuccess: Function,// eslint-disable-line
+    onJsonSuccess: Function
+  },
+  data() {
+    return {
+      loading: false,
+      excelData: {
+        header: null,
+        results: null
+      }
+    }
+  },
+  methods: {
+    // 导入的excel数据
+    generateData({ header, results }) {
+      this.excelData.header = header
+      this.excelData.results = results
+      this.onSuccess && this.onSuccess(this.excelData)
     },
-    data() {
-      return {
-        loading: false,
-        excelData: {
-          header: null,
-          results: null
-        }
+    // 导入json数据
+    generateJson(sheet) {
+      this.onJsonSuccess && this.onJsonSuccess(sheet)
+    },
+    handleUpload() {
+      this.$refs['excel-upload-input'].click()
+    },
+    handleClick(e) {
+      const files = e.target.files
+      const rawFile = files[0] // only use files[0]
+      if (!rawFile) return
+      this.upload(rawFile)
+    },
+    upload(rawFile) {
+      this.$refs['excel-upload-input'].value = null // fix can't select the same excel
+      if (!this.beforeUpload) {
+        this.readerData(rawFile)
+        return
+      }
+      const before = this.beforeUpload(rawFile)
+      if (before) {
+        this.readerData(rawFile)
       }
     },
-    methods: {
-      generateData({ header, results }) {
-        this.excelData.header = header
-        this.excelData.results = results
-        this.onSuccess && this.onSuccess(this.excelData)
-      },
-      handleDrop(e) {
-        e.stopPropagation()
-        e.preventDefault()
-        if (this.loading) return
-        const files = e.dataTransfer.files
-        if (files.length !== 1) {
-          this.$message.error('Only support uploading one file!')
-          return
-        }
-        const rawFile = files[0] // only use files[0]
-  
-        if (!this.isExcel(rawFile)) {
-          this.$message.error('Only supports upload .xlsx, .xls, .csv suffix files')
-          return false
-        }
-        this.upload(rawFile)
-        e.stopPropagation()
-        e.preventDefault()
-      },
-      handleDragover(e) {
-        e.stopPropagation()
-        e.preventDefault()
-        e.dataTransfer.dropEffect = 'copy'
-      },
-      handleUpload() {
-        this.$refs['excel-upload-input'].click()
-      },
-      handleClick(e) {
-        const files = e.target.files
-        const rawFile = files[0] // only use files[0]
-        if (!rawFile) return
-        this.upload(rawFile)
-      },
-      upload(rawFile) {
-        this.$refs['excel-upload-input'].value = null // fix can't select the same excel
-  
-        if (!this.beforeUpload) {
-          this.readerData(rawFile)
-          return
-        }
-        const before = this.beforeUpload(rawFile)
-        if (before) {
-          this.readerData(rawFile)
-        }
-      },
-      readerData(rawFile) {
-        this.loading = true
-        return new Promise((resolve, reject) => {
-          const reader = new FileReader()
+    readerData(rawFile) {
+      this.loading = true
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        if (this.isExcel(rawFile)) {
           reader.onload = e => {
             const data = e.target.result
             const workbook = XLSX.read(data, { type: 'array' })
@@ -80,25 +62,38 @@ Vue.component('upload-excel-component', {
             resolve()
           }
           reader.readAsArrayBuffer(rawFile)
-        })
-      },
-      getHeaderRow(sheet) {
-        const headers = []
-        const range = XLSX.utils.decode_range(sheet['!ref'])
-        let C
-        const R = range.s.r
-        /* start in the first row */
-        for (C = range.s.c; C <= range.e.c; ++C) { /* walk every column in the range */
-          const cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })]
-          /* find the cell in the first row */
-          let hdr = 'UNKNOWN ' + C // <-- replace with your desired default
-          if (cell && cell.t) hdr = XLSX.utils.format_cell(cell)
-          headers.push(hdr)
+        } else {
+          reader.readAsText(rawFile, "UTF-8");//读取文件
+          reader.onload = (e) => { //读取完文件之后会回来这里
+            const excelItems = JSON.parse(e.target.result); // 读取文件内容
+            const sheet = XLSX.utils.json_to_sheet(excelItems);
+            this.onJsonSuccess && this.onJsonSuccess(sheet)
+            this.loading = false
+            resolve()
+          }
         }
-        return headers
-      },
-      isExcel(file) {
-        return /\.(xlsx|xls|csv)$/.test(file.name)
+      })
+    },
+    getHeaderRow(sheet) {
+      const headers = []
+      const range = XLSX.utils.decode_range(sheet['!ref'])
+      let C
+      const R = range.s.r
+      /* start in the first row */
+      for (C = range.s.c; C <= range.e.c; ++C) { /* walk every column in the range */
+        const cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })]
+        /* find the cell in the first row */
+        let hdr = 'UNKNOWN ' + C // <-- replace with your desired default
+        if (cell && cell.t) hdr = XLSX.utils.format_cell(cell)
+        headers.push(hdr)
       }
+      return headers
+    },
+    isExcel(file) {
+      return /\.(xlsx|xls|csv)$/.test(file.name)
+    },
+    isJson() {
+      return /\.json$/.test(file.name)
     }
+  }
 })
